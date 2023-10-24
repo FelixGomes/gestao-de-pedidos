d69c1769622c46048f450aa731560ae59a3e4981
FelixGomes <gustavogomes50@gmail.com>
2023-10-24 00:01:03 -0300

    update classes

--- Models/Pedido.cs
+++ Models/Pedido.cs
@@ -1,12 +1,24 @@
-﻿namespace gestao_pedidos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace gestao_pedidos.Models
 {
     public class Pedido
     {
+        [Key]
+        [Display(Name = "Id")]
         public int PedidoId { get; set; }
+
+        [Display(Name = "Cliente Id")]
         public int ClienteId { get; set; } // Referência ao cliente
         public List<ItemPedido> Itens { get; set; } // Lista de itens do pedido
+
+        [Display(Name = "Valor Total")]
         public decimal ValorTotal { get; set; }
+
+        [Display(Name = "Data de Entrega")]
         public DateTime DataEntrega { get; set; }
+
+        [Display(Name = "Forma de Pagamento")]
         public string FormaDePagamento { get; set; }
     }
 }
--- Models/Produto.cs
+++ Models/Produto.cs
@@ -7,8 +7,14 @@ namespace gestao_pedidos.Models
         [Key]
         [Display(Name = "Id")]
         public int ProdutoId { get; set; }
+
+        [Display(Name = "Descricao")]
         public string Descricao { get; set; }
+
+        [Display(Name = "Valor Unitario")]
         public decimal ValorUnitario { get; set; }
+
+        [Display(Name = "Qtde em Estoque")]
         public int QuantidadeEmEstoque { get; set; }
     }
 }
